273a0ea04911f499c793151281b6df36f027ab88
Marc Tournadre <marc.tournadre@dynamixyz.com>
2017-11-09 16:18:24 +0100

    Started a new structure test

--- TensorMap.h
+++ TensorMap.h
@@ -296,6 +296,321 @@ public:
     { return contract<dim-2>(); };
 };
 
+template< typename Scalar, size_t dim >
+class TensorMapBase2
+{
+    template< typename OtherScalar, size_t OtherDim, size_t OtherCurrentDim >
+    friend class TensorMap2;
+
+protected:
+    Scalar *data_;
+    size_t shape_[dim];
+    size_t stride_[dim];
+
+    TensorMapBase2<Scalar,dim>& operator=( const TensorMapBase2& ) = delete;
+    TensorMapBase2<Scalar,dim>& operator+=( const TensorMapBase2& ) = delete;
+    TensorMapBase2<Scalar,dim>& operator-=( const TensorMapBase2& ) = delete;
+    TensorMapBase2<Scalar,dim>& operator<<( const TensorMapBase2& ) = delete;
+};
+
+/*
+template< typename Scalar >
+class TensorMapBase2<Scalar,1> : public Eigen::Map< ConstAs<Scalar, Vector<NonConst<Scalar>>>, Eigen::Unaligned, Eigen::InnerStride<Eigen::Dynamic> >
+{
+    template< typename OtherScalar, size_t OtherDim, size_t OtherCurrentDim >
+    friend class TensorMap2;
+
+protected:
+    Scalar *data_;
+    size_t shape_[1];
+    size_t stride_[1];
+};
+*/
+
+template< typename Scalar, size_t dim, size_t current_dim >
+class TensorMap2 : public TensorMapBase2<Scalar,dim>
+{
+    typedef Eigen::Ref<ConstAs<Scalar, MatrixR<NonConst<Scalar>>>,
+            Eigen::Unaligned, DynStride >
+            EigenRef;
+
+    static_assert( dim > 0, "You are a weird person" );
+    static_assert( current_dim <= dim, "You probably called operator() too many times" );
+
+    template< typename OtherScalar, size_t OtherDim, size_t OtherCurrentDim >
+    friend class TensorMap2;
+
+public:
+    TensorMap2()
+    {
+        this->data_ = NULL;
+    }
+
+    // Dimension constructors
+    template<typename ... Dimensions>
+    TensorMap2(Scalar *data, const DynInnerStride &inner_stride, Dimensions ... dimensions)
+    {
+        static_assert(sizeof...(dimensions) == dim, "You passed a wrong number of dimensions");
+        this->data_ = data;
+        this->stride_[dim - 1] = inner_stride.inner();
+        _init<0>(dimensions...);
+    }
+    template<typename ... Dimensions>
+    TensorMap2(Scalar *data, Dimensions ... dimensions)
+     : TensorMap2(data, DynInnerStride(1), dimensions...)
+    {}
+
+    // Copy and move constructors
+    TensorMap2( ConstAs< Scalar, TensorMapBase2< NonConst<Scalar>, dim> >& other )
+    {
+        this->data_ = other.data_;
+        std::copy( other.stride_, other.stride_ + dim, this->stride_ );
+        std::copy( other.shape_, other.shape_ + dim, this->shape_ );
+    }
+    TensorMap2( ConstAs< Scalar, TensorMapBase2< NonConst<Scalar>, dim> >&& other)
+    {
+        this->data_ = other.data_;
+        std::copy(other.stride_, other.stride_ + dim, this->stride_ );
+        std::copy(other.shape_, other.shape_ + dim, this->shape_ );
+    }
+
+    template< typename Base = const TensorMapBase2< Scalar, dim > >
+    TensorMap2( typename std::enable_if< std::is_const<Scalar>::value, Base >::type& other )
+    {
+        this->data_ = other.data_;
+        std::copy( other.stride_, other.stride_ + dim, this->stride_ );
+        std::copy( other.shape_, other.shape_ + dim, this->shape_ );
+    }
+
+protected:
+    // Slicing construction
+    template<size_t SliceDim>
+    TensorMap2(const Slice<SliceDim> &slice, ConstAs<Scalar,TensorMapBase2<Scalar,dim+1>>& super)
+    {
+        static_assert(SliceDim <= dim, "Slice used on invalid dimension");
+        assert(slice.idx < super.shape_[SliceDim] && "Index out of shape");
+        this->data_ = super.data_ + slice.idx * super.stride_[SliceDim];
+        std::copy(super.stride_, super.stride_ + SliceDim, this->stride_);
+        std::copy(super.shape_, super.shape_ + SliceDim, this->shape_);
+        std::copy(super.stride_ + SliceDim + 1, super.stride_ + dim + 1, this->stride_ + SliceDim);
+        std::copy(super.shape_ + SliceDim + 1, super.shape_ + dim + 1, this->shape_ + SliceDim);
+    }
+    template<size_t SliceDim>
+    TensorMap2(const Slice<SliceDim> &slice, ConstAs<Scalar,TensorMapBase2<Scalar,dim+1>>&& super)
+    {
+        static_assert(SliceDim < dim, "Slice used on invalid dimension");
+        assert(slice.idx < super.shape_[SliceDim] && "Index out of shape");
+        this->data_ = super.data_ + slice.idx * super.stride_[SliceDim];
+        std::copy(super.stride_, super.stride_ + SliceDim, this->stride_);
+        std::copy(super.shape_, super.shape_ + SliceDim, this->shape_);
+        std::copy(super.stride_ + SliceDim + 1, super.stride_ + dim + 1, this->stride_ + SliceDim);
+        std::copy(super.shape_ + SliceDim + 1, super.shape_ + dim + 1, this->shape_ + SliceDim);
+    }
+
+    // Contraction construction (contracts ContractDim with ContractDim+1 dimensions)
+    template<size_t ContractDim,
+            typename OtherScalar, size_t OtherCurrentDim,
+            typename SuperTensor = typename std::enable_if< std::is_const<Scalar>::value || !std::is_const<OtherScalar>::value,
+                    ConstAs<Scalar,TensorMap2<OtherScalar,dim+1,OtherCurrentDim>> >::type >
+    TensorMap2(const Contraction<ContractDim>&, SuperTensor& super)
+    {
+        static_assert(ContractDim < dim, "Contraction used on invalid dimension");
+        assert( super.stride_[ContractDim] == super.stride_[ContractDim+1] * super.shape_[ContractDim+1] &&
+                "Cannot trivially contract these dimensions" );
+        this->data_ = super.data_;
+
+        std::copy(super.stride_, super.stride_ + ContractDim, this->stride_);
+        std::copy(super.stride_ + ContractDim + 1, super.stride_ + dim + 1, this->stride_ + ContractDim);
+
+        std::copy(super.shape_, super.shape_ + ContractDim, this->shape_);
+        this->shape_[ContractDim] = super.shape_[ContractDim] * super.shape_[ContractDim+1];
+        std::copy(super.shape_ + ContractDim + 2, super.shape_ + dim + 1, this->shape_ + ContractDim+1);
+    }
+    template<size_t ContractDim,
+            typename OtherScalar, size_t OtherCurrentDim,
+            typename SuperTensor = typename std::enable_if< std::is_const<Scalar>::value || !std::is_const<OtherScalar>::value,
+                    TensorMap2<OtherScalar,dim+1,OtherCurrentDim> >::type >
+    TensorMap2(const Contraction<ContractDim>&, SuperTensor&& super)
+    {
+        static_assert(ContractDim < dim, "Contraction used on invalid dimension");
+        assert( super.stride_[ContractDim] == super.stride_[ContractDim+1] * super.shape_[ContractDim+1] &&
+                "Cannot trivially contract these dimensions" );
+        this->data_ = super.data_;
+
+        std::copy(super.stride_, super.stride_ + ContractDim, this->stride_);
+        std::copy(super.stride_ + ContractDim + 1, super.stride_ + dim + 1, this->stride_ + ContractDim);
+
+        std::copy(super.shape_, super.shape_ + ContractDim, this->shape_);
+        this->shape_[ContractDim] = super.shape_[ContractDim] * super.shape_[ContractDim+1];
+        std::copy(super.shape_ + ContractDim + 2, super.shape_ + dim + 1, this->shape_ + ContractDim+1);
+    }
+
+public:
+    // This is a bit dirty, but mainly used for binding numpy arrays
+    template< typename Integer >
+    TensorMap2( Scalar* data, const Shape<Integer>& shape, const Stride<Integer>& stride )
+    {
+        this->data_ = data;
+        for ( size_t i = 0 ; i < dim ; ++i )
+        {
+            this->shape_[i] = shape.shape[i];
+            this->stride_[i] = stride.stride[i];
+        }
+    }
+
+protected:
+    // Used to initialize strides and shapes in constructors
+    template<size_t s, typename ... OtherDimensions>
+    inline void _init(size_t d, OtherDimensions ... other_dimensions)
+    {
+        this->shape_[s] = d;
+        _init<s + 1>(other_dimensions...);
+        this->stride_[s] = this->shape_[s + 1] * this->stride_[s + 1];
+    }
+
+    template<size_t s>
+    inline void _init(size_t d)
+    {
+        this->shape_[s] = d;
+    }
+
+public:
+    // Returns a slice, along SliceDim
+    template<size_t SliceDim>
+    TensorMap2<Const<Scalar>,dim-1,current_dim> slice(size_t idx) const
+    { return TensorMap2<Const<Scalar>,dim-1,current_dim>(Slice<SliceDim>(idx),*this); }
+
+    template<size_t SliceDim>
+    TensorMap2<Scalar,dim-1,current_dim> slice(size_t idx)
+    { return TensorMap2<Scalar,dim-1,current_dim>(Slice<SliceDim>(idx),*this); }
+
+
+    // --- Utility methods ---
+
+    bool ravelable() const
+    {
+        for (size_t i = 0; i < dim - 1; ++i)
+        {
+            if (this->stride_[i] != this->shape_[i + 1] * this->stride_[i + 1])
+                return false;
+        }
+        return true;
+    }
+
+    size_t size() const
+    {
+        size_t s = 1;
+        for (size_t i = 0; i < dim ; ++i)
+            s *= this->shape_[i];
+        return s;
+    }
+
+    Eigen::Map<Vector<NonConst<Scalar>>, Eigen::Unaligned, DynInnerStride>
+    ravel()
+    {
+        assert(ravelable() && "Cannot be raveled");
+        return Eigen::Map<Vector<NonConst<Scalar>>,
+                Eigen::Unaligned, DynInnerStride>(this->data_, size(), DynInnerStride(this->stride_[dim - 1]));
+    }
+    Eigen::Map<const Vector<NonConst<Scalar>>, Eigen::Unaligned, DynInnerStride>
+    ravel() const
+    {
+        assert(ravelable() && "Cannot be raveled");
+        return Eigen::Map<const Vector<NonConst<Scalar>>,
+                Eigen::Unaligned, DynInnerStride>(this->data_, size(), DynInnerStride(this->stride_[dim - 1]));
+    }
+
+    size_t shape(size_t i) const { return this->shape_[i]; }
+    size_t stride(size_t i) const { return this->stride_[i]; }
+    Scalar* data() { return this->data_; }
+    Const<Scalar>* data() const { return this->data_; }
+
+    template< size_t NewDim, typename ... Dimensions >
+    TensorMap2<Scalar,NewDim,0>
+    reshape( Dimensions ... dimensions )
+    { return TensorMap2<Scalar,NewDim,0>( this->data_, dimensions... ); }
+
+    template< size_t NewDim, typename ... Dimensions >
+    TensorMap2<Const<Scalar>,NewDim,0>
+    reshape( Dimensions ... dimensions ) const
+    { return TensorMap2<Const<Scalar>,NewDim,0>( this->data_, dimensions... ); }
+
+    // Contracts ContractDim with ContractDim+1 dimensions
+    template< size_t ContractDim >
+    TensorMap2<Scalar,dim-1,0>
+    contract()
+    {
+        assert(ravelable() && "Cannot be trivially contracted");
+        return TensorMap<Scalar,dim-1>( Contraction<ContractDim>(), *this );
+    };
+    template< size_t ContractDim >
+    TensorMap<Const<Scalar>,dim-1,0>
+    contract() const
+    {
+        assert(ravelable() && "Cannot be trivially contracted");
+        return TensorMap<Const<Scalar>,dim-1>( Contraction<ContractDim>(), *this );
+    };
+
+    TensorMap<Scalar,dim-1,0> contractFirst()
+    { return contract<0>(); };
+    TensorMap<Const<Scalar>,dim-1,0> contractFirst() const
+    { return contract<0>(); };
+
+    TensorMap<Scalar,dim-1,0> contractLast()
+    { return contract<dim-2>(); };
+    TensorMap<Const<Scalar>,dim-1> contractLast() const
+    { return contract<dim-2,0>(); };
+
+    template< typename OtherTensor = TensorMap2< Scalar, dim, current_dim+1 > >
+    typename std::enable_if< (current_dim<=dim), OtherTensor >::type
+    operator()( void )
+    { return OtherTensor( *this ); }
+
+    template< typename OtherTensor = TensorMap2< Const<Scalar>, dim, current_dim+1 > >
+    typename std::enable_if< (current_dim<=dim), OtherTensor >::type
+    operator()( void ) const
+    { return OtherTensor( *this ); }
+
+
+    template< typename Index = size_t >
+    Scalar
+    operator()( typename std::enable_if< (current_dim<dim) && (dim==1), Index >::type i ) const
+    {
+        assert( i < this->shape_[0] && "Invalid index" );
+        return this->data_[ i * this->stride_[0]];
+    };
+
+    template< typename Index = size_t >
+    Scalar&
+    operator()( typename std::enable_if< (current_dim<dim) && (dim==1), Index >::type i )
+    {
+        assert( i < this->shape_[0] && "Invalid index" );
+        return this->data_[ i * this->stride_[0]];
+    };
+
+
+    template< typename Index = size_t >
+    TensorMap2< Scalar, dim-1, current_dim >
+    operator()( typename std::enable_if< (current_dim<dim) && (dim>1), Index >::type i )
+    { return TensorMap2< Scalar, dim-1, current_dim >( Slice<current_dim>(i), *this ); }
+
+    template< typename Index = size_t >
+    TensorMap2< Const<Scalar>, dim-1, current_dim >
+    operator()( typename std::enable_if< (current_dim<dim) && (dim>1), Index >::type i ) const
+    { return TensorMap2< Scalar, dim-1, current_dim >( Slice<current_dim>(i), *this ); }
+
+
+    template< typename ... OtherIndices >
+    typename std::conditional< (dim-sizeof...(OtherIndices) > 0), TensorMap<Scalar,dim-1,current_dim>, Scalar& >::type
+    operator()( typename std::enable_if< (current_dim<dim-sizeof...(OtherIndices)), size_t>::type i, OtherIndices ... indices )
+    { return this->operator()(i)(indices...); }
+
+    template< typename ... OtherIndices >
+    typename std::conditional< (dim-sizeof...(OtherIndices) > 0), TensorMap<Const<Scalar>,dim-1,current_dim>, Scalar >::type
+    operator()( typename std::enable_if< (sizeof...(OtherIndices) > 1 && current_dim<dim-sizeof...(OtherIndices)), size_t>::type i, OtherIndices ... indices ) const
+    { return this->operator()(i)(indices...); }
+};
+
 // ----------------------------------------------------------------------------------------
 
 #define TENSOR_MAP_BASE(Derived, Scalar, dim) TensorMapBase< \
--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ void test_schemes()
     InheritScheme<int> x4( a );
 }
 
-int main( int argc, char** argv )
+int main_old( int argc, char** argv )
 {
     test_schemes();
 
@@ -122,6 +122,95 @@ int main( int argc, char** argv )
     Eigen::Map< MatrixR<float,8,2> >( m.data() ) << v2, v2;
     Eigen::Map< MatrixR<float,8,2> >( bidule.data() ) << v2, v2;
     t.ravel().setZero();
+
+    return 0;
+}
+
+template< typename Scalar, size_t dim >
+using TensorMap2 = TensorMapTools::TensorMap2<Scalar,dim,0>;
+
+int main( int argc, char** argv )
+{
+    test_schemes();
+
+    // Const and construction tests
+    MatrixR<float,4,4> m = MatrixR<float,4,4>::Constant(42);
+    Eigen::Map< const MatrixR<float,4,4> > const_map( m.data() );
+    TensorMap2<float,3> t1( m.data(), 4, 2, 2 );
+    TensorMap2<float,3> t( m.data(), Eigen::InnerStride<>(m.innerStride()), 4, 2, 2 );
+    const TensorMap2<const float,3> t2( m.data(), 4, 2, 2 );
+    TensorMap2<const float,3> t3( const_map.data(), Eigen::InnerStride<>(1), 4, 2, 2 );
+
+    //TensorMap2<float,3> t4( const_map.data(), 4, 2, 2 ); // Error
+    //TensorMap2<float,3> t5( m, 4, 2, 3 ); // Runtime Error
+    //TensorMap2<float,4> t6( m, 4, 2, 2 ); // Error
+    //TensorMap2<float,0> t7( m ); // Error
+    //TensorMap2<float,2> sub1( t ); // Error
+
+    TensorMap2<float,3> t8 = t;
+    TensorMap2<const float,3> aze(t);
+    TensorMap2<const float,3> t9 = TensorMap2<float,3>(t);
+    TensorMap2<const float,3> t10 = TensorMap2<const float,3>(t);
+
+
+    // Slicing
+    //TensorMap2<float,2> sub2( Slice<3>(0), t );  // Error
+    //t.slice<1>( 3 ); // Runtime error
+    TensorMap2<float,2> sub3 = t.slice<1>(1);
+    TensorMap2<const float,2> sub4 = t.slice<1>(1);
+    t2.slice<1>(1);
+    TensorMap2<const float,2> sub5 = t2.slice<0>(0);
+
+    // operator()( void )
+    auto o1 = t();
+    auto o2 = t()();
+    auto o3 = t()()();
+    //auto o4 = t()()()(); // Error
+    TensorMap2<float,3> r1 = o3;
+    TensorMap2<const float,3> r2( o3 );
+    const TensorMap2<const float,3> r3( o2 );
+    o2();
+    r3()()();
+    // operator()( size_t )
+    t(0)()();
+    //t(0)()()(); // Error
+    t(0)(0)();
+    t(0)()(0);
+    const TensorMap2<const float,1> i1( t(0)()(1) );
+    //i1(3) = 2; // Error
+    t(0)(0)(0) = 1;
+
+    //t(0)()(0) = 1; // Error
+    Vector<float,2> v = t(0)(0);
+    t(0)(0) = v;
+    t(0)()(0) = 2*t()(0)(0).topRows(2);
+    /*
+    t(0) += MatrixR<float,2,2>::Zero();
+    t(0)(0,0) = 3;
+    t(0)(0) = t(0)(0)();
+
+    // operator()( ... )
+    t(0,0)();
+    t(0,0)(0);
+    t(0)(0,0);
+    t(0,0,0);
+    t()(0,0);
+
+    t.stride(0);
+    t.reshape<4>(2,2,2,2)()(1)()(0);
+
+    auto machin = t.reshape<4>(2,2,2,2).contract<1>();
+    auto truc = t.contractFirst();
+    //Vector<float,8> v2 = t.reshape<3>(4,2,2).contractFirst()()(0);
+    //Vector<float,8> v2 = Vector<float, 8>::Ones();
+    Vector<float> v2 = Vector<float>::Ones(8);
+    MatrixR<float> m3(8,2); m3 << v2, v2;
+    //auto bidule = t.reshape<2>(8,2);
+    TensorMap2<float,2> bidule( m.data(), 8, 2);
+    Eigen::Map< MatrixR<float,8,2> >( m.data() ) << v2, v2;
+    Eigen::Map< MatrixR<float,8,2> >( bidule.data() ) << v2, v2;
+    t.ravel().setZero();
+     */
 
     return 0;
 }
